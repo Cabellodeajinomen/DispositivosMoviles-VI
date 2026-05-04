add12ee1a3bfdb678d70fd4b6d1c53f2c63024e3
Alexx Rp <alexrhoddopacheco@gmail.com>
2026-05-03 21:29:09 -0500

    navegacion de rutas

---
+++ Prctica6/app/src/main/java/com/example/prctica6/navigation/Rutas.kt
@@ -0,0 +1,14 @@
+/**
+ * Descripcion corta: Centraliza las rutas de navegacion de la app para evitar errores por textos repetidos.
+ * Autor: Alex Rhoddo P
+ * Fecha de creacion: 2026-05-03
+ * Fecha de ultima modificacion: 2026-05-03
+ */
+package com.example.prctica6.navigation
+
+// Rutas de navegacion usadas por NavHost para cambiar entre pantallas.
+object Rutas {
+    const val ListaContactos = "lista_contactos"
+    const val FormularioContacto = "formulario_contacto"
+}
+
