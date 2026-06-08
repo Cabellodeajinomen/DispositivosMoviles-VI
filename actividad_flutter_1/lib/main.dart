73784bfead0f74b2fb4fe5733bde18f780ee7f15
Alexx Rp <alexrhoddopacheco@gmail.com>
2026-06-08 00:17:32 -0500

    Main de la aplicacion

---
+++ actividad_flutter_1/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'app/app.dart';
+
+// fecha inicio: 2026-06-07
+// fecha ultimo cambio: 2026-06-07
+// creado por: Alex RP
+// descripcion: inicia la app.
+
+void main() {
+  runApp(const MiApp());
+}
